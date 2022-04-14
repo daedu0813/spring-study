e10928a925ba0f41bee9b15cf21f6b6f670c2e2d
KI HOON KIM <daedu0813@gmail.com>
2022-04-14 15:11:37 +0900

    220414 spring study start

---
+++ src/main/java/com/tistory/daedu4it/springstudy/SpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.daedu4it.springstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tistory/daedu4it/springstudy/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.daedu4it.springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
